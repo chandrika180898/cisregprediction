94664b8b16b56cb516f90dadb884937f0835b56d
chandrika180898 <chandrikagummadi1@gmail.com>
2025-03-13 00:09:53 -0400

    Update c.py

--- c.py
+++ c.py
@@ -65,7 +65,7 @@ elif page == "Upload & Analyze":
         "Bipartite G-Quadruplex": re.compile(r'(G{3}N{1,3}G{3}N{1,3}G{3})N{1,7}(G{3}N{1,3}G{3}N{1,3}G{3})'),
         "G-Triplex DNA (G3-DNA)": re.compile(r'(G{3}N{1,7}){2}G{3}'),
         "G-Hairpin": re.compile(r'(G{3,})N{1,7}(G{3,})'),
-        "G-Guanine Slip-Strand DNA": re.compile(r'(GGG){3,}')
+        "G-Guanine Slip-Strand DNA": re.compile(r'(GGG){3,}'),
         "A-Tract": re.compile(r'(A{7,})'),
 
     }
